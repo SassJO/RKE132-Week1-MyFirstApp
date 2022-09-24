9ab3306588f7947c0c7c39afbf5c366f7723fbc6
SassJO <sassjorgen@gmail.com>
2022-09-24 11:30:53 +0300

    new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// This is a comment to my code
+
+//rakendus kusib kasutajalt sisestada tema nime
+//rakendus tervitab kasutaja nimepidi
+
+Console.WriteLine("enter your name:");//output
+
+//string-sone
+string userName = Console.ReadLine(); //input
+
+
+
+Console.WriteLine("Hello" + ", " + userName + "!");//output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //output
